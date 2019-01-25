84a7f0bf5635e2871b2f399a9bed5fa1fb7c889a
AshishDmss <ashishk@dmss.co.in>
2019-01-25 15:50:37 +0530

    update

--- app/src/main/java/common/AppController.java
+++ app/src/main/java/common/AppController.java
@@ -1,6 +1,10 @@
 package common;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import utils.Configuration;
 
@@ -26,5 +30,18 @@ public class AppController extends Application {
 
     public void logout() {
         manager.setUserLoggedIn(false);
+    }
+
+    public static String getSharedPrefs(Context c) {
+        try {
+            Context con = c.createPackageContext("com.sportsfight.com.sportsfight", Context.MODE_PRIVATE);
+            SharedPreferences pref = con.getSharedPreferences("demopref", Context.MODE_PRIVATE);
+            String your_data =
+                    pref.getString("demostring", "No Value");
+            return your_data;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("Not data shared", e.toString());
+        }
+        return "";
     }
 }
