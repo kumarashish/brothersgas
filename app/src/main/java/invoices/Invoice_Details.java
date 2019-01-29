63f3af819f9b8bdd3af9261eaefbefb63bbc3e02
ashish kumar <ak.ashishyadav001@gmail.com>
2019-01-29 08:57:13 +0530

    my commmit

--- app/src/main/java/invoices/Invoice_Details.java
+++ app/src/main/java/invoices/Invoice_Details.java
@@ -1,4 +1,21 @@
 package invoices;
 
-public class Invoice_Details {
-}
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.brothersgas.R;
+
+import common.AppController;
+import common.WebServiceAcess;
+
+public class Invoice_Details extends Activity {
+    WebServiceAcess webServiceAcess;
+    AppController controller;
+    String contractId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_list);
+
+    }}
